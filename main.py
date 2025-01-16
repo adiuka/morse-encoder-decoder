ff0897c1128f4bcd6ced830763eb6f956f299d56
adiuka <amartinaitiz@gmail.com>
2025-01-16 11:10:32 +0100

    finish function and main

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ while program_on:
                         "turns them into morse coded strings or vice versa. Please choose a mode (encode or decode, and exit): \n")
 
     if mode_selection == 'encode':
-        message = input("Please enter the message you would like to be coded in morse: \n")
+        message = input("Please enter the message you would like to be coded in morse: \n\n")
         print(f.encode_message(message=message))
 
     elif mode_selection == 'decode':
         message = input("Please enter the message in morse you would like to be translated: \n"
                         "(NOTE: Morse letters contain . and -, each letter is seperated by / and each word by a ' ') \n"
-                        "(e.g. )")
+                        "(e.g. '...././.-../.-../--- .--/---/.-./.-../-../-.-.--') \n \n")
         try: 
-            print(message)
+            print(f.decode_message(message))
+        except KeyError:
+            print("You have made a mistake in the message, please check the instructions on a correct morse message: \n"
+                  "(Morse letters contain . and -, each letter is seperated by / and each word by a ' ') \n"
+                  "(e.g. '...././.-../.-../--- .--/---/.-./.-../-../-.-.--')\n \n")
+            mode_selection = 'decode'
+    elif mode_selection == 'exit':
+        print("Thank you for using the morse code encoder/decoder!")
+        program_on = False
+    else:
+        print("Invalid mode, please choose encode, decode or exit.\n")
